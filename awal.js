ec8c9a60ef120ecde5b9e874d517752b21fb6b4b
akbarrmdn <akbarramadhaan12@gmail.com>
2023-06-11 12:25:14 +0700

    OOP

---
+++ awal.js
@@ -0,0 +1,25 @@
+console.log("Hello World");
+
+// @TODO: Cetak teks "Saya mulai belajar JavaScript!" ke console
+console.log("Saya mulai belajar JavaScript!");
+
+let fullName = (lastName = "Skywalker");
+console.log(fullName);
+
+// First Quiss
+/**
+ * TODO:
+ * Buatlah variabel firstName, lastName, age, isMarried dengan ketentuan:
+ *  - firstName: bertipe data string, dengan nilai "John".
+ *  - lastName: bertipe data string, dengan nilai "Doe".
+ *  - age: bertipe data number, dengan nilai 25.
+ *  - isMarried: bertipe data boolean, dengan nilai true.
+ */
+
+// Tulis kode di bawah ini
+let firstName = "John";
+let lastName = "Doe";
+let age = 25;
+let isMarried = true;
+
+console.log(firstName, lastName, age, isMarried);
---
+++ tes.js
@@ -0,0 +1,10 @@
+if ((true || false) && (false || false)) {
+    console.log("It's true");
+} else {
+    console.log("It's false");
+}
+
+const name = 'Dicoding';
+const language = 'JavaScript';
+
+console.log(`Hello ${name}. Welcome to ${language}!`);
